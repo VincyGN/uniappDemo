05e701c3ce6a944486bd6c9888d43407c060a247
VincyGN <gnclaire0022@163.com>
2021-09-28 22:05:07 +0800

    01-配置tabBar

---
+++ pages/category/category.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		categorayddddd
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
